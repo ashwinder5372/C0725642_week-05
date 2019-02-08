75b563363be6a68ba0f8cd6224cf6d3e0b4e3d59
visualstudio <visualstudio@122-PC08>
2019-02-08 18:18:08 -0500

    6:10

--- C0725642/Program.cs
+++ C0725642/Program.cs
@@ -12,4 +12,31 @@ namespace C0725642
         {
         }
     }
+    class Node
+    {
+        public Node() { }
+        public Node floor;
+        public Node elevatorup;
+        public string floorNumber;
+
+    }
+    class Elevator
+    {
+        Node FirstFloor;
+        Node SecondFloor;
+        Node ThirdFloor;
+        Node FourthFloor;
+
+        public void run()
+        {
+            FirstFloor = new Node();
+            FirstFloor.floorNumber = "First Floor";
+            Console.WriteLine("Floor number is {0}", FirstFloor.floorNumber);
+            FirstFloor.elevatorup = SecondFloor;
+            SecondFloor.floorNumber = "Second Floor";
+
+            Console.WriteLine("floor Number is {0}", SecondFloor.floorNumber);
+
+        }
+    }
 }
